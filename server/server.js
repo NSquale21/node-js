38aa86a5f84ddac32386bcd784b99829d8b13169
Nicole Pasquale <NSquale212@gmail.com>
2020-02-05 18:02:08 -0600

    Add part 1 objectives

--- server/server.js
+++ server/server.js
@@ -1,2 +1,8 @@
 const path = require('path');
 const fs = require('fs');
+
+fs.readFile(path.join(__dirname, '../chirps.json'), (err, data) => {
+    if (err) console.log(err);
+    let parsed = JSON.parse(data);
+    parsed.chirps.forEach(chirp => console.log(chirp.message));
+});
