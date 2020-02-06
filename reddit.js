b16576c200d200896a900dd791963ad173520207
Nicole Pasquale <NSquale212@gmail.com>
2020-02-06 10:01:00 -0600

    Add part 2 objectives

---
+++ reddit.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const fs = require('fs');
+const rp = require('request-promise');
+
+rp('https://reddit.com/r/popular.json')
+.then(res => {
+	
+	let popular = JSON.parse(res);
+
+	let popularArticles = popular.data.children.map(article => {
+		return {
+			title : article.data.title
+		}
+	});
+	
+	fs.writeFile(path.join(__dirname, './popular-articles.json'), JSON.stringify(popularArticles), err => {
+		if (err) console.log(err); 
+	});
+}).catch(err => console.log(err));
